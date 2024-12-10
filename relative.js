ef8609281caf62a00ef7c521d396bfe6ead7cecf
MorvainForStudy <master_pokhot@outlook.com>
2024-12-10 09:38:41 -0300

    acho que finalizei

---
+++ relative.js
@@ -0,0 +1,8 @@
+function expansive() {
+    let display = document.getElementById('display');
+    if (display.style.display == 'block') {
+        display.style.display = 'none';
+    } else {
+        display.style.display = 'block';
+    }
+}
